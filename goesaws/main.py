c99b0fd2220187d6756ebcdcdff468f93088e871
mnichol3 <nicholson.matt94@gmail.com>
2019-08-29 19:13:50 -0400

    Add GLM data functionality
    * Add the ability to query & download GLM data files
    * Improve functionality elsewhere

--- goesaws/main.py
+++ goesaws/main.py
@@ -8,10 +8,14 @@ conn = goesawsinterface.GoesAWSInterface()
 
 # Returns a list of AwsGoesFile objects representing AWS
                                                # product      # start date       # end date     # sector  # channel
-imgs = conn.get_avail_images_in_range('goes16', 'ABI-L2-CMIP', '8-6-2019-15:00', '8-6-2019-15:10', 'M2', '13')
+# imgs = conn.get_avail_images_in_range('goes16', 'ABI-L2-CMIP', '8-6-2019-15:00', '8-6-2019-15:10', 'M2', '13')
+imgs = conn.get_avail_images_in_range('goes16', 'glm', '8-6-2019-15:00', '8-6-2019-15:10')
+
+# imgs = conn.get_avail_images_in_range('goes16', 'ABI-L2-TPW', '5-23-2019-21:00', '5-23-2019-22:00', 'M2', '13') # TPW unsupported
+
 for img in imgs:
     print('{} --> {}'.format(img.scan_time, img.filename))
 
-result = conn.download('goes16', imgs, local_abi_path, keep_aws_folders=False, threads=6)
-for x in result._successfiles:
-    print(x.filepath)
+# result = conn.download('goes16', imgs, local_abi_path, keep_aws_folders=False, threads=6)
+# for x in result._successfiles:
+#     print(x.filepath)
