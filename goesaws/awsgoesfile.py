5788e8c502a823c082a382902a9be90f96788e2b
mnichol3 <nicholson.matt94@gmail.com>
2019-10-21 12:12:57 -0400

    Add files lost in merge

---
+++ goesaws/awsgoesfile.py
@@ -0,0 +1,40 @@
+import os
+import re
+
+from datetime import datetime
+
+
+class AwsGoesFile(object):
+
+    def __init__(self, key, shortfname, scan_time):
+        super(AwsGoesFile, self).__init__()
+        self.key = key
+        self.shortfname = shortfname
+        self.scan_time = scan_time
+        self.awspath = None
+        self.filename = None
+        if self.key is not None:
+            self._parse_key()
+
+
+
+    def _parse_key(self):
+        self.awspath, self.filename = os.path.split(self.key)
+
+
+
+    def _create_filepath(self, basepath, keep_aws_structure):
+        if keep_aws_structure:
+            directorypath = os.path.join(basepath, self.awspath.split('/', 1)[1])
+            filepath = os.path.join(directorypath, self.filename)
+        else:
+            directorypath = basepath
+            filepath = os.path.join(basepath, self.filename)
+
+        return directorypath,filepath
+
+
+
+
+    def __repr__(self):
+        return '<AwsGoesFile object - {}>'.format(self.shortfname)
